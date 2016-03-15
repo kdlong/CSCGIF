5103cfb309c94f4cfb158e22bab9e89eebe90876
Kenneth Long <kdlong@hep.wisc.edu>
2016-03-15 17:16:25 +0100

    Plot data from multiple files

--- plotGasGains.py
+++ plotGasGains.py
@@ -2,16 +2,21 @@
 import ROOT
 import argparse
 import os
+import glob
+import errno
 
+config_info = {}
 def getComLineArgs():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file_name", required=True,
-            help="EDM file name (should be full path, starting"
-                "with '/store' for file on DAS"
+    parser.add_argument("-f", "--input_folder", required=True,
+            help="Folder containing data files to plot"
     )
     parser.add_argument("--discard_fits", action='store_true',
             help="Don't store plots for fits to exponential "
                 "fits (for pA points)"
+    )
+    parser.add_argument("--logy", action='store_true',
+            help="Use log scale for y-axis"
     )
     parser.add_argument("-o", "--output_folder", type=str,
             default="/afs/cern.ch/user/k/kelong/www/GIFstuff",
@@ -26,7 +31,14 @@ def readDataFromFile(file_name):
         for line in data_file:
             if line[0] in ["#"]:
                 continue
-            file_info.append([time(x) for x in line.split()])
+            elif "=" in line:
+                (key, value) = line.split("=")
+                config_info[key.strip()] = value.strip()
+                continue
+            data = line.split()
+            if len(data) < 3:
+                continue
+            file_info.append([time(x) for x in data])
         entries = {x : 
             {"currents" : [],
                 "times": [],
@@ -34,24 +46,22 @@ def readDataFromFile(file_name):
             "stable current" : 0,
             "stable error" : 0
             }
-            for x in set([i[0] for i in file_info])
+            for x in set([i[0] for i in file_info if len(i) > 0])
         }
     for entry in file_info:
         entries[entry[0]]["times"].append(entry[1])
         entries[entry[0]]["currents"].append(entry[2])
-        entries[entry[0]]["errors"].append(0 if len(entry) < 3 else entry[3])
+        entries[entry[0]]["errors"].append(0 if len(entry) < 4 else entry[3])
     return entries
 def fitVoltagePoint(voltage, points, output_dir):
     canvas = ROOT.TCanvas("v%i" % int(voltage))
     if output_dir != "" and not os.path.isdir(output_dir):
-        os.mkdir(output_dir)
+        makeDirectory(output_dir)
     graph = ROOT.TGraphErrors(len(points))
     graph.SetName("V=%s" % int(voltage))
     graph.GetXaxis().SetTitle("Time (m)")
     graph.GetYaxis().SetTitle("Current (nA)")
     graph.SetMarkerStyle(20)
-    graph.GetXaxis().SetTitle("Applied Voltage (V)")
-    graph.GetYaxis().SetTitle("Current (nA)")
     offset = points[0][0]
     for j, point in enumerate(points):
         graph.SetPoint(j, point[0] - offset, point[1])
@@ -65,17 +75,28 @@ def fitVoltagePoint(voltage, points, output_dir):
     if os.path.isdir(output_dir):
         graph.Draw("AP")
         graph.GetXaxis().SetRangeUser(-5, points[-1][0] - offset + 5)
+        graph.GetXaxis().SetTitle("Current (nA)")
+        graph.GetYaxis().SetTitle("Time (min)")
         canvas.Print("%s/v%s.pdf" % (output_dir, int(voltage)))
     return (function.GetParameter(0), function.GetParError(0))
-def main():
-    data_path = "/afs/cern.ch/cms/MUON/csc/fast1-test-ISR/IVmeasurements/GasGain"
-    args = getComLineArgs()
-    entries = readDataFromFile("/".join([data_path, args.file_name]))
-    final_canvas = ROOT.TCanvas("final", "final")
+def makeDirectory(path):
+    '''
+    Make a directory, don't crash
+    '''
+    try:
+        os.makedirs(path)
+    except OSError as e:
+        if e.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else: 
+            raise
+def getCurve(data_file, output_dir):
+    entries = readDataFromFile(data_file)
     total_graph = ROOT.TGraphErrors(len(entries))
-    total_graph.SetName("Total")
-    output_dir = "/".join([args.output_folder, 
-                    "GasGain", args.file_name.split("/")[0]])
+    voltages = entries.keys()
+    voltages.sort()
+    print voltages
+    total_graph.SetName("_".join(["curve", str(voltages[0]), str(voltages[-1])]))
     for i, (key, value) in enumerate(entries.iteritems()):
         if len(value["currents"]) == 1:
             value["stable current"] = value["currents"][0]
@@ -85,16 +106,42 @@ def main():
                     zip(value["times"], value["currents"], value["errors"]),
                     "/".join([output_dir, "pAFits"])
                 )
+        # Correction factor, using averages from 
+        corr_voltage = key - value["stable current"]*0.000104
         total_graph.SetPoint(i, key, value["stable current"])
         total_graph.SetPointError(i, 0, value["stable error"]) 
-    final_canvas.cd()
-    final_canvas.SetLogy()
     total_graph.GetXaxis().SetTitle("Applied Voltage (V)")
     total_graph.GetYaxis().SetTitle("Current (nA)")
     total_graph.SetMinimum(1)
+    total_graph.SetMaximum(max([v["stable current"] for k, v in entries.iteritems()]))
     total_graph.SetMarkerStyle(20)
     total_graph.SetMarkerSize(1)
-    total_graph.Draw("AP")
+    return total_graph
+def main():
+    data_path = "/afs/cern.ch/cms/MUON/csc/fast1-test-ISR/IVmeasurements/GasGain"
+    final_canvas = ROOT.TCanvas("final", "final")
+    args = getComLineArgs()
+    colors = [ROOT.kRed, ROOT.kBlue, ROOT.kBlack, ROOT.kGreen]
+    graphs = []
+    output_dir = "/".join([args.output_folder,
+        "GasGain", args.input_folder])
+    makeDirectory(output_dir)
+    for i, data_file in enumerate(glob.glob("/".join([data_path, args.input_folder, "*"]))):
+        graphs.append(getCurve(data_file, args.output_folder))
+    ymax = 1.3*max([x.GetMaximum() for x in graphs])
+    xmax = max([x.GetXaxis().GetXmax() for x in graphs])
+    print "Xmax is %s" % xmax
+    print "Ymax is %s" % ymax
+    for i, graph in enumerate(graphs):    
+        graph.SetMarkerColor(colors[i])
+        if i == 0:
+            graph.GetXaxis().SetLimits(0, xmax)
+            graph.SetMaximum(ymax)
+            graph.Draw("AP")
+        else:
+            graph.Draw("Psames")
+    if args.logy:
+        final_canvas.SetLogy()
     final_canvas.Print("/".join([output_dir, "final.pdf"]))
 
 if __name__ == "__main__":
